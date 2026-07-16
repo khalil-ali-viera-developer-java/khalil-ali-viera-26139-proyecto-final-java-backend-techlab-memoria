014b107531ec534daba61922297ea400d30d5351
khalil <khalil.ali.viera@gmail.com>
2026-07-16 09:29:26 -0300

    Master 1° commit

---
+++ src/test/java/com/elviejomolino/gestorarticulos/GestorArticulosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elviejomolino.gestorarticulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorArticulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
