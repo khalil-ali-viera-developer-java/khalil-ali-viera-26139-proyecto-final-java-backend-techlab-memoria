62fe5e1f9504b4db7018f628854df167a64eca49
khalil <khalil.ali.viera@gmail.com>
2026-07-20 18:53:32 -0300

    Master 7° commit: En la paquete exception agregue la clase ArticuloNotFoundException que extendera de Excepcion que me obligara a capturar posible excepciones

--- src/main/java/com/elviejomolino/gestorarticulos/exception/ArticuloNotFoundException.java
+++ src/main/java/com/elviejomolino/gestorarticulos/exception/ArticuloNotFoundException.java
@@ -1,5 +1,18 @@
 package com.elviejomolino.gestorarticulos.exception;
 
-public class ArticuloNotFoundException {
+public class ArticuloNotFoundException extends Exception { // INICIO CLASE ARTICULONOTFOUNDEXCEPTION;
 
-}
+    // ESTADO: ATRIBUTOS;
+
+    // COMPORTAMIENTO: CONSTRUCTORES - MÉTODOS;
+    // CONSTRUCTOR POR DEFECTO;
+    public ArticuloNotFoundException() {
+
+    }
+
+    // CONSTRUCTOR PARAMETRIZADO;
+    public ArticuloNotFoundException(String mensaje) {
+        super(mensaje);
+    }
+
+} // FINAL CLASE ARTICULONOTFOUNDEXCEPTION;
--- src/main/java/com/elviejomolino/gestorarticulos/service/ArticuloService.java
+++ src/main/java/com/elviejomolino/gestorarticulos/service/ArticuloService.java
@@ -1,9 +1,16 @@
 package com.elviejomolino.gestorarticulos.service;
 
+// IMPORTO LIST;
+import java.util.List;
+
+// IMPORTO ARTICULO;
+import com.elviejomolino.gestorarticulos.model.Articulo;
+
 public interface ArticuloService { // INICIO INTERFACE ARTICULOSERVICE;
 
     // MÉTODOS;
     // FINDALL();
+    List<Articulo> findAllService();
 
     // FINDBYID(ID);
 
