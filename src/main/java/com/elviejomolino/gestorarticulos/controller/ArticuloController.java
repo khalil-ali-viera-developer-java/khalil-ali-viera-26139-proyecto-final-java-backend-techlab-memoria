88682a7d2d5a696c53728126396b4efddaa92850
khalil <khalil.ali.viera@gmail.com>
2026-07-23 21:20:21 -0300

    Master 12° commit: En el paquete controller agrego la clase ArticuloController. Donde tengo una descripción para poder completarla

---
+++ src/main/java/com/elviejomolino/gestorarticulos/controller/ArticuloController.java
@@ -0,0 +1,32 @@
+package com.elviejomolino.gestorarticulos.controller;
+
+// IMPORTO REQUESTMAPPING;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// IMPORTO RESTCONTROLLER;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/memoria/articulos")
+
+public class ArticuloController { // INICIO CLASE ARTICULOCONTROLLER;
+
+    // ESTADO: ATRIBUTOS;
+
+    // COMPORTAMIENTO: CONTRUCTORES - MÉTODOS;
+    // CONSTRUCTOR POR DEFECTO;
+
+    // CONSTRUCTOR PARAMETRIZADO;
+
+    // MÉTODOS;
+    // FINDALL();
+
+    // FINDBYID(ID);
+
+    // SAVE(ENTITY);
+
+    // MODIFYBYID(ID, ENTITY);
+
+    // DELETEBYID(ID);
+
+} // FINAL CLASE ARTICULOCONTROLLER;
